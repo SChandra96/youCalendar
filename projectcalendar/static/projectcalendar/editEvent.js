bf5346b0b97daa2fe9f56c1670e4e5818d25da47
Maggie Lin <linym012@163.com>
2017-03-28 22:36:59 -0400

    Add checkbox uncheck js

--- projectcalendar/static/projectcalendar/editEvent.js
+++ projectcalendar/static/projectcalendar/editEvent.js
@@ -1,22 +1,5 @@
-function popRepeatPage(checkbox) {
-	if(checkbox.checked == true){
-		alert("Hello!");
-	}else{
-		document.getElementById("submit").addAttribute("disabled");
-   }
+function unCheckRepeat(){
+	$('#repeat').attr('checked', false)
+
+
 }
-
-// function popRepeatPage2(checkbox) {
-// 	if(checkbox.checked == true){
-// 		$('#myModal').modal('show');
-// 	}else{
-// 		document.getElementById("submit").addAttribute("disabled");
-//    }
-// }
-
-
-// ​$("[id^=repeat2]").on("change", function(e){
-// 	if(e.target.checked){
-// 	$('#myModal').modal();
-// 	}
-// });
