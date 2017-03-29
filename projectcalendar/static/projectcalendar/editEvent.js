eb4f424aeab53a535cef21ebb285af96a58493c7
Maggie Lin <linym012@163.com>
2017-03-28 22:17:19 -0400

    Add modal pop-up when check on checkbox

--- projectcalendar/static/projectcalendar/editEvent.js
+++ projectcalendar/static/projectcalendar/editEvent.js
@@ -1,7 +1,22 @@
 function popRepeatPage(checkbox) {
-    if(checkbox.checked == true){
-        alert("Hello!");
-    }else{
-        document.getElementById("submit").addAttribute("disabled");
+	if(checkbox.checked == true){
+		alert("Hello!");
+	}else{
+		document.getElementById("submit").addAttribute("disabled");
    }
 }
+
+// function popRepeatPage2(checkbox) {
+// 	if(checkbox.checked == true){
+// 		$('#myModal').modal('show');
+// 	}else{
+// 		document.getElementById("submit").addAttribute("disabled");
+//    }
+// }
+
+
+// ​$("[id^=repeat2]").on("change", function(e){
+// 	if(e.target.checked){
+// 	$('#myModal').modal();
+// 	}
+// });
